c4c3bb5dd02f0a09d67bc1a4bff124e34d13bfbb
Mansur <ahmatowmansur@gmail.com>
2021-11-05 23:41:26 +0300

    новый проект для API с поддержкой razor

---
+++ SiteDownChecker.API/Controllers/Item controllers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteDownChecker.Business.Models;
+
+namespace SiteDownChecker.API.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class AreaController : EntityController<Area,Area>
+    {
+    }
+
+    [ApiController, Route("[controller]")]
+    public class BodyPartController : EntityController<BodyPart, BodyPart>
+    {
+    }
+
+    [ApiController, Route("[controller]")]
+    public class DisiagesController : EntityController<API.Models.Disease, Disease>
+    {
+    }
+
+    [ApiController, Route("[controller]")]
+    public class LimbController : EntityController<Limb, Limb>
+    {
+    }
+
+    [ApiController, Route("[controller]")]
+    public class VideoController : EntityController<Video, Video>
+    {
+    }
+}
