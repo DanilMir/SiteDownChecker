fc50395bb66bda75d25384b01ce7c638d1ed8283
Мансур Ахматов <ahmatowmansur@gmail.com>
2021-12-19 14:20:22 +0300

    q

--- SiteDownChecker.Web/Controllers/HomeController.cs
+++ SiteDownChecker.Web/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SiteDownChecker.Web.DB;
 
 namespace SiteDownChecker.Web.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly SiteDownContext _dataContext;
+    public HomeController(SiteDownContext dataContext) => 
+        _dataContext = dataContext;
+    public IActionResult Index() => View(_dataContext.Sites);
     public IActionResult Privacy() => View();
 }
