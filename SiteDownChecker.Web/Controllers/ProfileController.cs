4c58039a1e8004aa06b3441838258bda86cb4c44
Мансур Ахматов <ahmatowmansur@gmail.com>
2021-12-19 14:00:37 +0300

    profile

---
+++ SiteDownChecker.Web/Controllers/ProfileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiteDownChecker.Web.Attributes;
+using SiteDownChecker.Web.DB;
+
+namespace SiteDownChecker.Web.Controllers;
+
+public class ProfileController : Controller
+{
+    private readonly SiteDownContext _dataContext;
+    public ProfileController(SiteDownContext dataContext) => 
+        _dataContext = dataContext;
+
+    [Authorize]
+    public IActionResult Index() => View(HttpContext.Items["User"]);
+}
