dee23458adaf3adbca976706e7d2440cd9f1afca
Дмитрий Баев <81518929+DmitryTXCMN@users.noreply.github.com>
2021-12-19 15:27:13 +0300

    SiteInfoAdded

---
+++ SiteDownChecker.Web/Views/SiteInfo/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteDownChecker.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private string block;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
