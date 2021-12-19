0d89884b78d3f24a853667b8d655499ae372b32d
Мансур Ахматов <ahmatowmansur@gmail.com>
2021-12-19 17:02:21 +0300

    q

--- SiteDownChecker.Web/Controllers/ProfileController.cs
+++ SiteDownChecker.Web/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using SiteDownChecker.Web.Attributes;
 using SiteDownChecker.Web.DB;
 
@@ -7,9 +8,18 @@ namespace SiteDownChecker.Web.Controllers;
 public class ProfileController : Controller
 {
     private readonly SiteDownContext _dataContext;
-    public ProfileController(SiteDownContext dataContext) => 
+
+    public ProfileController(SiteDownContext dataContext) =>
         _dataContext = dataContext;
 
     [Authorize]
-    public IActionResult Index() => View(HttpContext.Items["User"]);
+    public IActionResult Index(int userId)
+    {
+        var user = userId is default(int)
+            ? HttpContext.Items["User"] as User
+            : _dataContext.Users.FirstOrDefault(u => u.Id == userId);
+        if (user is null)
+            return BadRequest();
+        return View(user);
+    }
 }
